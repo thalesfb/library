5ae8ead955d95ed48be3393d26d03e118affc12b
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 10:27:49 -0300

    fix: Update LoanRepository to use book and person fields for querying

--- biblioteca/src/main/java/com/ifc/library/repositories/LoanRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/LoanRepository.java
@@ -1,17 +1,20 @@
 package com.ifc.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.ifc.library.entity.Loan;
-
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, String> {
   
-    Optional<Loan> findByTitle(String title);
-    Optional<Loan> findByIsbn(String isbn);
-    Optional<Loan> findByEmail(String email);
-    List<Loan> findLoanByEmail(String email);
+    Optional<Loan> findByBookTitle(String title);
+    Optional<Loan> findByBookIsbn(String isbn);
+    Optional<Loan> findByPersonEmail(String email);
+    // @Query("SELECT l FROM Loan l WHERE l.user.email = :email")
+    // List<Loan> findByUserEmail(@Param("email") String email);
+    List<Loan> findAll();
 }
