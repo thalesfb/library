9c2bce5770314266c5169da0a1d8b0921fe2b1ca
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 10:21:35 -0300

    feat: Add deleteByIsbn method to BookRepository

--- biblioteca/src/main/java/com/ifc/library/repositories/BookRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/BookRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ifc.library.entity.Book;
-
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +12,6 @@ public interface BookRepository extends JpaRepository<Book, String> {
 
     Optional<Book> findByTitle(String title);
     Optional<Book> findByIsbn(String isbn);
+    Optional<Book> deleteByIsbn(String isbn);
+    List<Book> findAll();
 }
