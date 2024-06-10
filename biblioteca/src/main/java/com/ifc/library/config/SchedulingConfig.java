875023e99e585a8dc5180336b69122edd58537cf
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 11:39:37 -0300

    feat: Add SchedulingConfig for enabling scheduling in the application

---
+++ biblioteca/src/main/java/com/ifc/library/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.ifc.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
