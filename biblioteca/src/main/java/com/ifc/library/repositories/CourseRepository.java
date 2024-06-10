353632bc7d89819128b65eaa6233525c1b61fcf5
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 10:25:34 -0300

    feat: Add findByName method to CourseRepository

--- biblioteca/src/main/java/com/ifc/library/repositories/CourseRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/CourseRepository.java
@@ -2,11 +2,13 @@ package com.ifc.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.ifc.library.entity.Course;
-
+import com.ifc.library.entity.Student;
 import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+
+    Optional<Course> findByName(String name);
+    Optional<Student> findStudent(String course);
 }
