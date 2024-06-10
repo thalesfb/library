8fef4e1d32f45c25f2a13f0bfd1341b24d93e103
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 10:37:29 -0300

    feat: Add additional methods to StudentRepository

--- biblioteca/src/main/java/com/ifc/library/repositories/StudentRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/StudentRepository.java
@@ -8,4 +8,13 @@ import com.ifc.library.entity.Student;
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, String> {}
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+    Optional<Student> findByName(String name);
+
+    Optional<Student> findByEmail(String email);
+
+    Optional<Student> findByCourse(String course);
+
+    Optional<Student> findByRegistration(String registration);
+}
