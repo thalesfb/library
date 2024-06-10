78a3e8d454d30d1e78bfcd6e165ca51f02de1157
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-10 15:10:30 -0300

    refactor: Remove unused findStudent method in CourseRepository

--- biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
+++ biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
@@ -4,25 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import java.util.Optional;
+import com.ifc.library.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import com.ifc.library.entity.User;
 import com.ifc.library.service.UserService;
 import com.ifc.library.dto.UserDTO;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    // listando livros
-    // @Autowired
-    // private UserService userService;
+    public final UserService userService;
+    public final UserRepository userRepository;
 
-    // @PostMapping("/book")
-    // public ResponseEntity<User> 
-
-    // listando emprestimos do usuario
+    @DeleteMapping("/delete/{email}")
+    public ResponseEntity<String> removeUser(@PathVariable String email) {
+        Optional<User> userOpt = this.userRepository.findByEmail(email);
+        if (userOpt.isPresent()) {
+            userService.removeUser(email);
+            return ResponseEntity.ok("User removed");
+        } else {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+    }
 }
