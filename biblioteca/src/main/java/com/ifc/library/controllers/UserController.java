d081d289234ebeca50a085ed9f72b4aaa8d45db4
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-12 00:28:56 -0300

    refactor: Update UserController to use shorter URL for deleting a user

--- biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
+++ biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.ifc.library.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
@@ -9,24 +7,29 @@ import com.ifc.library.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import com.ifc.library.entity.User;
 import com.ifc.library.service.UserService;
+import com.ifc.library.dto.LoginRequestDTO;
 import com.ifc.library.dto.UserDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    public final UserService userService;
-    public final UserRepository userRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @DeleteMapping("/{email}")
-    public ResponseEntity<String> removeUser(@PathVariable String email) {
-        Optional<User> userOpt = this.userRepository.findByEmail(email);
-        if (userOpt.isPresent()) {
-            userService.removeUser(email);
-            return ResponseEntity.ok("User removed");
-        } else {
+    @DeleteMapping("/")
+    public ResponseEntity removeUser(@RequestBody LoginRequestDTO body) {
+        Optional<User> userOpt = this.userRepository.findByEmail(body.email());
+        if(userOpt.isEmpty()) {
             return ResponseEntity.badRequest().body("User not found");
+        } else if(passwordEncoder.matches(body.password(), userOpt.get().getPassword())) {
+                userService.removeUser(body.email());
+                return ResponseEntity.ok("User removed");
+        } else {
+                return ResponseEntity.badRequest().body("Invalid password");
         }
     }
 }
