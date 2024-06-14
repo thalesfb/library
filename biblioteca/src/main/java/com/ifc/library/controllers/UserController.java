93fd0dc8d07ebdac96f281d32f3d445c1a9c5fd7
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-14 08:44:52 -0300

    refactor: Update UserController to use UserService for deleting a user

--- biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
+++ biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.ifc.library.repositories.UserRepository;
+import com.ifc.library.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -13,14 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @DeleteMapping("/{email}")
     public ResponseEntity<Void> removeUser(@PathVariable String email) {
-        if(userRepository.findByEmail(email)) {
-            return ResponseEntity.noContent().build();
-        }
-        userRepository.deleteByEmail(email);
-        return ResponseEntity.notFound().build();
+        if(userService.findByEmail(email) == null){
+            return ResponseEntity.notFound().build();
+            }
+        userService.removeUser(email);
+        return ResponseEntity.noContent().build();
     }
 }
