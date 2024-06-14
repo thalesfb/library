e2dd08a2c7d72961462376d3f25cd536131981c4
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-14 11:14:26 -0300

    refactor: Add @Transactional and @Modifying annotations to deleteByEmail method in UserRepository

--- biblioteca/src/main/java/com/ifc/library/repositories/UserRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.ifc.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import com.ifc.library.entity.User;
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +17,9 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmailAndPassword(String email, String password);
 
     Optional<User> findByEmail(String email);
-
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.email = :email")
     Optional<User> deleteByEmail(String email);
 
     Optional<User> findByName(String name);
