9f894f0914b18aaf2ef17cee69be702429f90dfe
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-14 11:13:41 -0300

    refactor: Add deleteByName method to AuthorRepository

--- biblioteca/src/main/java/com/ifc/library/repositories/AuthorRepository.java
+++ biblioteca/src/main/java/com/ifc/library/repositories/AuthorRepository.java
@@ -3,14 +3,18 @@ package com.ifc.library.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.ifc.library.entity.Author;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, String> {
   
     Optional<Author> findByName(String name);
-
-    Optional<Author> deleteByName(String name);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Author a WHERE a.name = :name")
+    void deleteByName(String name);
 }
