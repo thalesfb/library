85ae9c878fa917c56a76f9fd9e2c0d746fbc9f52
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-13 23:29:20 -0300

    refactor: Update UserController to use shorter URL for deleting a user

--- biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
+++ biblioteca/src/main/java/com/ifc/library/controllers/UserController.java
@@ -1,19 +1,11 @@
 package com.ifc.library.controllers;
 
-import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ifc.library.dto.LoginRequestDTO;
-import com.ifc.library.entity.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.ifc.library.repositories.UserRepository;
-import com.ifc.library.service.UserService;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,20 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    @DeleteMapping("/")
-    public ResponseEntity removeUser(@RequestBody LoginRequestDTO body) {
-        Optional<User> userOpt = this.userRepository.findByEmail(body.email());
-        if(userOpt.isEmpty()) {
-            return ResponseEntity.badRequest().body("User not found");
-        } else if(passwordEncoder.matches(body.password(), userOpt.get().getPassword())) {
-                userService.removeUser(body.email());
-                return ResponseEntity.ok("User removed");
-        } else {
-                return ResponseEntity.badRequest().body("Invalid password");
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> removeUser(@PathVariable String email) {
+        if(userRepository.findByEmail(email)) {
+            return ResponseEntity.noContent().build();
         }
+        userRepository.deleteByEmail(email);
+        return ResponseEntity.notFound().build();
     }
 }
